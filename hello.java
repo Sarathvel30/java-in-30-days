e65d9012bd2129b3f6f94815fb3404f5a2f0416e
Sarathvel K V <157451287+Sarathvel30@users.noreply.github.com>
2026-06-24 19:18:24 +0530

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class hello{
+    public static void main(String agrs[])
+    {
+        Scanner values=new Scanner(System.in);
+        
+        String b=values.nextLine();
+        System.out.print(b);
+    }
+}

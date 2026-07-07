58c456fbd1101081ce6454732fafad5d27ba9913
Sarathvel K V <157451287+Sarathvel30@users.noreply.github.com>
2026-07-07 20:20:17 +0530

    Add files via upload

---
+++ ex1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class ex1{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int mark=sc.nextInt();
+        if(mark>=35){
+            System.out.println("pass");
+        }else{
+            System.out.print("fail");
+        }
+    }
+}

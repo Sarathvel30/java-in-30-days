3d7a654c656df9e8869b98828334facb011efe5a
Sarathvel K V <157451287+Sarathvel30@users.noreply.github.com>
2026-07-10 01:22:41 +0530

    Add files via upload

---
+++ ifelseq2.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+class ifelseq2{
+    public static void main(String arg[]){
+        Scanner sc=new Scanner(System.in);
+        String meghana=sc.nextLine();
+        if(meghana.equals("dead")){
+            System.out.println("suriya meets ramya");
+        }else{
+            System.out.println("surya weds meghana");
+        }
+    }
+}
